f276f3b0817035d79c204e5ed9d0c9b81f7ff6f3
mkatrodia <45700458+mkatrodia@users.noreply.github.com>
2020-06-28 23:07:53 -0400

    initial commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+
+})
+
+export class PostCreateComponent {
+  //porperty
+  enteredValue ='';
+  newPost = '';
+
+  //method
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+
+})
+
+export class PostListComponent {
+  // posts = [
+  //   {name: 'Dave Masko', age:"30"},
+  //   {name: 'Phili Joe', age:"33"},
+  //   {name: 'Juli Rahe', age:"39"},
+  //   {name: 'Jus Jew', age:"47"}
+  // ];
+  posts = [];
+
+}
